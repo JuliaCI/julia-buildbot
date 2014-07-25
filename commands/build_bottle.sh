694018ec4dabcd9ad9261f4306d19b8bce61f393
Elliot Saba <staticfloat@gmail.com>
2014-07-25 12:26:28 -0400

    Only uninstall the deps, fool

--- commands/build_bottle.sh
+++ commands/build_bottle.sh
@@ -20,7 +20,7 @@ fi
 $brew update
 
 # Remove everything first, so we always start clean
-$brew rm $($brew list) 2>/dev/null
+$brew rm $($brew deps $FORMULA) 2>/dev/null
 
 # Install dependencies first as bottles when possible
 deps=$($brew deps -n $1)
