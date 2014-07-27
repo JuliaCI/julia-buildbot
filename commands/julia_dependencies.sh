23bd3927c062ab57d364273439bba49fcd103b5e
Elliot Saba <staticfloat@gmail.com>
2014-07-27 14:10:39 -0400

    Looks like we need full paths here as well

--- commands/julia_dependencies.sh
+++ commands/julia_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # We do this because if we run the command straight from buildbot, we're inside of a virtualenv jail
-brew install -v --only-dependencies julia
+/usr/local/bin/brew install -v --only-dependencies julia
