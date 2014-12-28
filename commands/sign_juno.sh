590e8b4706245a08c20aa9f142d25c6239790ee8
Elliot Saba <staticfloat@gmail.com>
2014-12-28 01:12:10 -0800

    Update juno signing process

--- commands/sign_juno.sh
+++ commands/sign_juno.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "Usage: $0 <osx url> <win url>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+	echo "Usage: $0 <osx64 url> <win32 url> <win64 url>"
 	exit 1
 fi
 
-OSX_URL="$1"
-WIN_URL="$2"
+OSX64_URL="$1"
+WIN32_URL="$2"
+WIN64_URL="$3"
 
 if [[ "$(uname)" == "Darwin" ]]; then
 	# First, clean everything out
-	rm -f juno*
+	rm -rf juno*
 	hdiutil detach /Volumes/Juno 2>/dev/null
 
 	# Next, download OSX .dmg
-	FILENAME=$(basename "$OSX_URL")
-	curl -L "$OSX_URL" > $FILENAME
+	FILENAME=$(basename "$OSX64_URL")
+	curl -L "$OSX64_URL" > $FILENAME
 
 	# Mount it as read-write
 	echo "Mounting $FILENAME..."
@@ -25,7 +26,6 @@ if [[ "$(uname)" == "Darwin" ]]; then
 	echo "Signing Juno.app..."
 	~/unlock_keychain.sh
 	codesign -f -s "AFB379C0B4CBD9DB9A762797FC2AB5460A2B0DBE" --deep /Volumes/Juno/Juno.app
-	codesign --deep --display --verbose=4 /Volumes/Juno/Juno.app
 	if [[ $? != 0 ]]; then
 		exit $?
 	fi
@@ -37,8 +37,16 @@ if [[ "$(uname)" == "Darwin" ]]; then
 	echo "Creating ${FILENAME%.*}-signed.dmg..."
 	hdiutil convert $FILENAME -format UDZO -o ${FILENAME%.*}-signed.dmg -shadow -imagekey zlib-level=9
 else
+	if [[ "$(hostname)" == "win81x86" ]]; then
+		WIN_URL="$WIN32_URL"
+		FOLDER_NAME="windows32"
+	else
+		WIN_URL="$WIN64_URL"
+		FOLDER_NAME="windows64"
+	fi
+
 	# First, clean everything out
-	rm -rf windows*
+	rm -rf juno* $FOLDER_NAME
 
 	# Next, download windows .zip
 	FILENAME=$(basename "$WIN_URL")
@@ -50,12 +58,12 @@ else
 
 	# Go into the windows directory (we assume that always exists), sign juno.exe
 	echo "Signing juno.exe..."
-	~/sign.sh windows/juno.exe
+	~/sign.sh $FOLDER_NAME/juno.exe
 	if [[ $? != 0 ]]; then
 		exit $?
 	fi
 
 	# Zip it up
 	echo "Zipping into ${FILENAME%.*}-signed.zip..."
-	powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('windows', '${FILENAME%.*}-signed.zip'); }"
+	powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('$FOLDER_NAME', '${FILENAME%.*}-signed.zip'); }"
 fi
