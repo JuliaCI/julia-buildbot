23382a64360513a1a97a045e07ac618572a088e2
Elliot Saba <staticfloat@gmail.com>
2015-11-24 11:10:19 -0800

    Add nightly threading builder

---
+++ master/nightly_threading.py
@@ -0,0 +1,124 @@
+###############################################################################
+# Define everything needed to build nightly Julia builds against LLVM SVN for .jl
+###############################################################################
+
+julia_threading_builders = ["nightly_threading64"]
+threading_nightly_scheduler = Nightly(name="Julia Threading package", builderNames=julia_threading_builders, hour=[0,12], branch="master", onlyIfChanged=True )
+c['schedulers'].append(threading_nightly_scheduler)
+
+threading_force_scheduler = ForceScheduler(
+    name="Julia Threading building",
+    builderNames=["nightly_threading64"],
+    reason=FixedParameter(name="reason", default=""),
+    branch=FixedParameter(name="branch", default=""),
+    repository=FixedParameter(name="repository", default=""),
+    project=FixedParameter(name="project", default="Juno"),
+    properties=[
+    ]
+)
+c['schedulers'].append(threading_force_scheduler)
+
+julia_threading_factory = BuildFactory()
+julia_threading_factory.useProgress = True
+julia_threading_factory.addSteps([
+    # Clone julia
+    Git(
+    	name="Julia checkout",
+    	repourl=Property('repository', default='git://github.com/JuliaLang/julia.git'),
+    	mode='incremental',
+    	method='clean',
+    	submodules=True,
+    	clobberOnFailure=True,
+    	progress=True
+    ),
+    # Fetch so that remote branches get updated as well.
+    ShellCommand(
+    	name="git fetch",
+    	command=["git", "fetch"],
+    	flunkOnFailure=False
+    ),
+
+    # Add our particular configuration to flags
+    SetPropertyFromCommand(
+        name="Add configuration to flags",
+        command=["echo", Interpolate("%(prop:flags)s JULIA_THREADS=1")],
+        property="flags"
+    ),
+
+    # make clean first, and nuke llvm
+    ShellCommand(
+    	name="make cleanall",
+    	command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s cleanall")]
+    ),
+
+    # Make!
+    ShellCommand(
+        name="make binary-dist",
+        command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s binary-dist")],
+        haltOnFailure = True
+    ),
+    SetPropertyFromCommand(
+        name="Get major/minor version",
+        command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor)\")"],
+        property="majmin"
+    ),
+    SetPropertyFromCommand(
+        name="Get major/minor/patch version",
+        command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor).$(VERSION.patch)\")"],
+        property="version"
+    ),
+    SetPropertyFromCommand(
+        name="Get shortcommit",
+        command=["./julia", "-e", "println(Base.GIT_VERSION_INFO.commit[1:10])"],
+        property="shortcommit"
+    ),
+
+    # Upload the result!
+    MasterShellCommand(
+        name="mkdir julia_package",
+        command=["mkdir", "-p", "/tmp/julia_package"]
+    ),
+    FileUpload(
+        slavesrc=Interpolate("julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz"),
+        masterdest=Interpolate("/tmp/julia_package/juliathreading-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")
+    ),
+
+    # Upload it to AWS and cleanup the master!
+    MasterShellCommand(
+        name="Upload to AWS",
+        command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/%(prop:majmin)s/juliathreading-%(prop:version)s-%(prop:shortcommit)s-linux%(prop:bits)s.tar.gz /tmp/julia_package/juliathreading-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
+        haltOnFailure=True
+    ),
+    MasterShellCommand(
+        name="Upload to AWS (latest)",
+        command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/juliathreading-latest-linux%(prop:bits)s.tar.gz /tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
+        doStepIf=is_nightly_build,
+        haltOnFailure=True
+    ),
+    MasterShellCommand(
+        name="Cleanup Master",
+        command=["rm", "-f", Interpolate("/tmp/julia_package/juliathreading-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")]
+    ),
+
+    MasterShellCommand(
+        name="Report success",
+        command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice curl -L -H 'Content-type: application/json' -d '{\"target\": \"linux_threading-%(prop:tar_arch)s\", \"url\": \"https://s3.amazonaws.com/julianightlies/bin/linux/%(prop:up_arch)s/%(prop:majmin)s/juliathreading-%(prop:version)s-%(prop:shortcommit)s-linux%(prop:bits)s.tar.gz\", \"version\": \"%(prop:shortcommit)s\"}' https://status.julialang.org/put/nightly")],
+        doStepIf=is_nightly_build
+    ),
+])
+
+
+# Add linux tarball builders
+#c['builders'].append(BuilderConfig(
+#    name="nightly_threading32",
+#    slavenames=["ubuntu12.04-x86"],
+#    category="Packaging",
+#    factory=julia_threading_factory
+#))
+
+c['builders'].append(BuilderConfig(
+    name="nightly_threading64",
+    slavenames=["centos6.7-x64"],
+    category="Nightlies",
+    factory=julia_threading_factory
+))
