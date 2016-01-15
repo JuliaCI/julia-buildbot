6f07505c7e7b7e9703d848a9aa1b6b64ebc14b66
Elliot Saba <staticfloat@gmail.com>
2016-01-15 11:05:29 -0800

    add `want_stderr=false` to julia invocations
    also make whitespace consistent in package_tarball.py

--- master/package_tarball.py
+++ master/package_tarball.py
@@ -21,43 +21,46 @@ julia_tarball_factory.addSteps([
 
     # Clone julia
     Git(
-    	name="Julia checkout",
-    	repourl=Property('repository', default='git://github.com/JuliaLang/julia.git'),
-    	mode='incremental',
-    	method='clean',
-    	submodules=True,
-    	clobberOnFailure=True,
-    	progress=True
+        name="Julia checkout",
+        repourl=Property('repository', default='git://github.com/JuliaLang/julia.git'),
+        mode='incremental',
+        method='clean',
+        submodules=True,
+        clobberOnFailure=True,
+        progress=True
     ),
 
     # make clean first
     ShellCommand(
-    	name="make cleanall",
-    	command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s cleanall")]
+        name="make cleanall",
+        command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s cleanall")]
     ),
 
     # Make!
     ShellCommand(
-    	name="make binary-dist",
-    	command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s binary-dist")],
-    	haltOnFailure = True
+        name="make binary-dist",
+        command=["/bin/bash", "-c", Interpolate("make %(prop:flags)s binary-dist")],
+        haltOnFailure = True
     ),
 
     # Set a bunch of properties that everyone will need
     SetPropertyFromCommand(
-    	name="Get major/minor version",
-    	command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor)\")"],
-    	property="majmin"
+        name="Get major/minor version",
+        command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor)\")"],
+        want_stderr=False,
+        property="majmin"
     ),
     SetPropertyFromCommand(
-    	name="Get major/minor/patch version",
-    	command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor).$(VERSION.patch)\")"],
-    	property="version"
+        name="Get major/minor/patch version",
+        command=["./julia", "-e", "println(\"$(VERSION.major).$(VERSION.minor).$(VERSION.patch)\")"],
+        want_stderr=False,
+        property="version"
     ),
     SetPropertyFromCommand(
-    	name="Get shortcommit",
-    	command=["./julia", "-e", "println(Base.GIT_VERSION_INFO.commit[1:10])"],
-    	property="shortcommit"
+        name="Get shortcommit",
+        command=["./julia", "-e", "println(Base.GIT_VERSION_INFO.commit[1:10])"],
+        want_stderr=False,
+        property="shortcommit"
     ),
     SetPropertyFromCommand(
         name="Get commitmessage",
@@ -87,35 +90,35 @@ julia_tarball_factory.addSteps([
 
     # Upload the result!
     MasterShellCommand(
-    	name="mkdir julia_package",
-    	command=["mkdir", "-p", "/tmp/julia_package"]
+        name="mkdir julia_package",
+        command=["mkdir", "-p", "/tmp/julia_package"]
     ),
     FileUpload(
-    	slavesrc=Interpolate("julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz"),
-    	masterdest=Interpolate("/tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")
+        slavesrc=Interpolate("julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz"),
+        masterdest=Interpolate("/tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")
     ),
 
     # Upload it to AWS and cleanup the master!
     MasterShellCommand(
-    	name="Upload to AWS",
-    	command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/%(prop:majmin)s/julia-%(prop:version)s-%(prop:shortcommit)s-linux%(prop:bits)s.tar.gz /tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
-    	haltOnFailure=True
+        name="Upload to AWS",
+        command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/%(prop:majmin)s/julia-%(prop:version)s-%(prop:shortcommit)s-linux%(prop:bits)s.tar.gz /tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
+        haltOnFailure=True
     ),
     MasterShellCommand(
-    	name="Upload to AWS (latest)",
-    	command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/julia-latest-linux%(prop:bits)s.tar.gz /tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
-    	doStepIf=is_nightly_build,
-    	haltOnFailure=True
+        name="Upload to AWS (latest)",
+        command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice ~/bin/aws put --fail --public julianightlies/bin/linux/%(prop:up_arch)s/julia-latest-linux%(prop:bits)s.tar.gz /tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")],
+        doStepIf=is_nightly_build,
+        haltOnFailure=True
     ),
     MasterShellCommand(
-    	name="Cleanup Master",
-    	command=["rm", "-f", Interpolate("/tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")]
+        name="Cleanup Master",
+        command=["rm", "-f", Interpolate("/tmp/julia_package/julia-%(prop:shortcommit)s-Linux-%(prop:tar_arch)s.tar.gz")]
     ),
 
     MasterShellCommand(
-    	name="Report success",
+        name="Report success",
         command=["/bin/bash", "-c", Interpolate("~/bin/try_thrice curl -L -H 'Content-type: application/json' -d '{\"target\": \"linux-%(prop:tar_arch)s\", \"url\": \"https://s3.amazonaws.com/julianightlies/bin/linux/%(prop:up_arch)s/%(prop:majmin)s/julia-%(prop:version)s-%(prop:shortcommit)s-linux%(prop:bits)s.tar.gz\", \"version\": \"%(prop:shortcommit)s\"}' https://status.julialang.org/put/nightly")],
-    	doStepIf=is_nightly_build
+        doStepIf=is_nightly_build
     ),
 
     # Trigger a download of this file onto another slave for coverage purposes
