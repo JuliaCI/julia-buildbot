f343823792a32136c2cd620a1f4e8a041152d6d7
Elliot Saba <staticfloat@gmail.com>
2016-09-26 20:18:06 -0700

    Always remove `libgfortran` before building bottles

--- commands/build_bottle.sh
+++ commands/build_bottle.sh
@@ -33,8 +33,9 @@ fi
 # Remove everything first, so we always start clean
 $brew rm --force $($brew deps $FORMULA) 2>/dev/null
 $brew rm --force $FORMULA 2>/dev/null
-#$brew cleanup --cached
-#rm -vf $($brew --cache)/*bottle.*tar.gz
+
+# Always remove this guy because he's bad news for bottles
+$brew rm --force libgfortran
 
 # Install dependencies first as bottles when possible
 deps=$($brew deps -n $1)
