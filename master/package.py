7e52c8f4c9d975c8e8a2b962d04d1ffa87989c25
Elliot Saba <staticfloat@gmail.com>
2016-11-29 16:19:07 -0800

    I need an extra strip()

--- master/package.py
+++ master/package.py
@@ -44,7 +44,7 @@ def parse_git_log(return_code, stdout, stderr):
 
 def parse_artifact_filename(return_code, stdout, stderr):
     if stdout[:26] == 'JULIA_BINARYDIST_FILENAME=':
-        artifact_filename = stdout[26:]
+        artifact_filename = stdout[26:].strip()
         if '-osx' in artifact_filename:
             artifact_filename += '.dmg'
         elif '-winnt' in artifact_filename:
