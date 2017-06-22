d3589864997d6273d34de3436dab05425a9342cd
Elliot Saba <staticfloat@gmail.com>
2017-06-22 16:50:46 -0700

    More debugging

--- master/github_listener.py
+++ master/github_listener.py
@@ -3,8 +3,11 @@ from buildbot.www.hooks.github import GitHubEventHandler
 class JuliaGithubListener(GitHubEventHandler):
     # In addition to all the other events, we parse release events as well
     # (for things like LLVM.jl, Cxx.jl, etc...)
-    def handle_release(self, payload):
+    def handle_release(self, payload, event):
+        print "payload"
         print payload
+        print "event"
+        print event
 
         # Do some magic here
         return [], 'git'
