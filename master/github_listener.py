cb11232e693e70e3afcb1bd67a7c98d1ceb6d84f
Elliot Saba <staticfloat@gmail.com>
2017-06-22 17:11:55 -0700

    First shot at parsing release event

--- master/github_listener.py
+++ master/github_listener.py
@@ -1,13 +1,23 @@
 from buildbot.www.hooks.github import GitHubEventHandler
+from dateutil.parser import parse as dateparse
+
 
 class JuliaGithubListener(GitHubEventHandler):
     # In addition to all the other events, we parse release events as well
     # (for things like LLVM.jl, Cxx.jl, etc...)
     def handle_release(self, payload, event):
-        print "payload"
-        print payload
-        print "event"
-        print event
+        if 'release' not in payload:
+            import json
+            payload = json.loads(payload['payload'][0])
+        
+        change = {
+            'repository': payload['repository']['url'],
+            'project': payload['repository']['full_name'],
+            'tag_name': payload['release']['tag_name'],
+            'revision': payload['release']['tag_name'],
+            'when_timestamp': dateparse(payload['release']['published_at']),
+            'revlink': payload['release']['url'],
+        }
 
         # Do some magic here
-        return [], 'git'
+        return [change], 'git'
