decc6981812b72b0d1b9c99afa35930905f4a65d
Elliot Saba <staticfloat@gmail.com>
2019-04-01 16:07:52 -0700

    Expand FreeBSD buildbot fleet

--- master/inventory.py
+++ master/inventory.py
@@ -28,7 +28,7 @@ linux_names    += build_names("linux", ["ppc64le"], ["osu_%d"%(idx) for idx in r
 linux_names    += build_names("linux", ["aarch64"], ["packet_%d"%(idx) for idx in range(1,9)])
 linux_names    += build_names("linux", ["armv7l"], ["firefly_%d"%(idx) for idx in range(1,4)])
 macos_names     = build_names("macos", ["x86_64"], ["macmini", "macmini2", "akatsuki"])
-freebsd_names   = build_names("freebsd", ["x86_64"], ["openstack_1"])
+freebsd_names   = build_names("freebsd", ["x86_64"], ["openstack_%d"%(idx) for idx in range(1,4)])
 all_names       = win_names + linux_names + macos_names + freebsd_names
 
 # Define all the attributes we'll use in our buildsteps
