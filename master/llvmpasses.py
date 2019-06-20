e4d626a06dde699c14bc3e50fce46085232a379f
Elliot Saba <staticfloat@gmail.com>
2019-06-20 14:23:16 -0700

    Add `llvmpasses_linux64` builder

---
+++ master/llvmpasses.py
@@ -0,0 +1,78 @@
+julia_llvmpasses_factory = util.BuildFactory()
+julia_llvmpasses_factory.useProgress = True
+julia_llvmpasses_factory.addSteps([
+    # Clone julia
+    steps.Git(
+        name="Julia checkout",
+        repourl=util.Property('repository', default='git://github.com/JuliaLang/julia.git'),
+        mode='full',
+        method='fresh',
+        submodules=True,
+        clobberOnFailure=True,
+        progress=True,
+        retryFetch=True,
+        getDescription={'--tags': True},
+    ),
+
+    # Make debug build
+    steps.ShellCommand(
+        name="make release",
+        command=["/bin/sh", "-c", util.Interpolate("%(prop:make_cmd)s -j%(prop:nthreads)s %(prop:flags)s %(prop:extra_make_flags)s release")],
+        haltOnFailure = True,
+        # Fail out if 60 minutes have gone by with nothing printed to stdout
+        timeout=60*60,
+        # Kill everything if the overall job has taken more than 2 hours
+        maxTime=60*60*2,
+    ),
+
+    steps.ShellCommand(
+        name="make test/llvmpasses",
+        command=["/bin/sh", "-c", util.Interpolate("%(prop:make_cmd)s -C test/llvmpasses -j%(prop:nthreads)s %(prop:flags)s %(prop:extra_make_flags)s")],
+        haltOnFailure = True,
+        # Fail out if 60 minutes have gone by with nothing printed to stdout
+        timeout=60*60,
+        # Kill everything if the overall job has taken more than 2 hours
+        maxTime=60*60*2,
+    ),
+])
+
+c['schedulers'].append(schedulers.AnyBranchScheduler(
+    name="Julia test llvmpasses",
+    change_filter=util.ChangeFilter(filter_fn=julia_ci_filter),
+    builderNames=["llvmpasses_linux64"],
+    treeStableTimer=1,
+))
+
+# Add workers for these jobs
+c['builders'].append(util.BuilderConfig(
+    name="llvmpasses_linux64",
+    workernames=builder_mapping["linux64"],
+    collapseRequests=False,
+    tags=["Packaging"],
+    factory=julia_llvmpasses_factory,
+))
+
+# Add a scheduler for building release candidates/triggering builds manually
+c['schedulers'].append(schedulers.ForceScheduler(
+    name="llvmpasses",
+    label="Force llvmpasses",
+    builderNames=["llvmpasses_linux64"],
+    reason=util.FixedParameter(name="reason", default=""),
+    codebases=[
+        util.CodebaseParameter(
+            "",
+            name="",
+            branch=util.FixedParameter(name="branch", default=""),
+            repository=util.FixedParameter(name="repository", default=""),
+            project=util.FixedParameter(name="project", default="Packaging"),
+        )
+    ],
+    properties=[
+        util.StringParameter(
+            name="extra_make_flags",
+            label="Extra Make Flags",
+            size=30,
+            default="",
+        ),
+    ],
+))
