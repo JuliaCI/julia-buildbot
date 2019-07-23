2a573a7499e227ba85b1fbcb81ba1bf33821877b
Elliot Saba <staticfloat@gmail.com>
2019-07-23 14:01:43 -0400

    Add GPG signing capabilities

--- master/builder_utils.py
+++ master/builder_utils.py
@@ -118,24 +118,33 @@ def munge_artifact_filename(props_obj):
 def render_upload_command(props_obj):
     upload_path = gen_upload_path(props_obj, namespace="pretesting")
     upload_filename = props_obj.getProperty("upload_filename")
-    return ["/bin/sh", "-c", "aws s3 cp --acl public-read /tmp/julia_package/%s s3://%s"%(upload_filename, upload_path)]
+    return ["/bin/sh", "-c",
+        "aws s3 cp --acl public-read /tmp/julia_package/%s s3://%s && "%(upload_filename, upload_path) +
+        "aws s3 cp --acl public-read /tmp/julia_package/%s.asc s3://%s.asc"%(upload_filename, upload_path),
+    ]
 
 @util.renderer
 def render_promotion_command(props_obj):
     src_path = gen_upload_path(props_obj, namespace="pretesting")
     dst_path = gen_upload_path(props_obj)
-    return ["/bin/sh", "-c", "aws s3 cp --acl public-read s3://%s s3://%s"%(src_path, dst_path)]
+    return ["/bin/sh", "-c",
+        "aws s3 cp --acl public-read s3://%s s3://%s && "%(src_path, dst_path) +
+        "aws s3 cp --acl public-read s3://%s.asc s3://%s.asc"%(src_path, dst_path),
+    ]
 
 @util.renderer
 def render_latest_promotion_command(props_obj):
     src_path = gen_upload_path(props_obj, namespace="pretesting")
     dst_path = gen_upload_path(props_obj, latest=True)
-    return ["/bin/sh", "-c", "aws s3 cp --acl public-read s3://%s s3://%s"%(src_path, dst_path)]
+    return ["/bin/sh", "-c",
+        "aws s3 cp --acl public-read s3://%s s3://%s && "%(src_path, dst_path) +
+        "aws s3 cp --acl public-read s3://%s.asc s3://%s.asc"%(src_path, dst_path),
+    ]
 
 @util.renderer
 def render_cleanup_pretesting_command(props_obj):
     del_path = gen_upload_path(props_obj, namespace="pretesting")
-    return ["/bin/sh", "-c", "aws s3 rm s3://%s"%(del_path)]
+    return ["/bin/sh", "-c", "aws s3 rm s3://%s s://%s.asc"%(del_path, del_path)]
 
 @util.renderer
 def render_download_url(props_obj):
