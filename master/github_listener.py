712dc080e8dd9c342e38f5a69d91836ac4a3f283
Elliot Saba <staticfloat@gmail.com>
2019-08-08 16:21:57 -0700

    Try to ingest `create` events for tags

--- master/github_listener.py
+++ master/github_listener.py
@@ -3,6 +3,25 @@ from dateutil.parser import parse as dateparse
 
 
 class JuliaGithubListener(GitHubEventHandler):
+    def handle_create(self, payload, event):
+        if 'ref_type' not in payload:
+            import json
+            payload = json.loads(payload['payload'][0])
+
+        if payload['ref_type'] != 'tag':
+            return [], 'git'
+        
+        change = {
+            'author': payload['sender']['login'],
+            'repository': payload['repository']['url'],
+            'project': payload['repository']['full_name'],
+            'revision': payload['ref'],
+            'category': 'tag',
+        }
+
+        # Do some magic here
+        return [change], 'git'
+
     # In addition to all the other events, we parse release events as well
     # (for things like LLVM.jl, Cxx.jl, etc...)
     def handle_release(self, payload, event):
