a8660b3e925b4f4a6a7ccf6eda983688d709d931
Elliot Saba <staticfloat@gmail.com>
2019-12-11 14:09:15 -0800

    Override github pull request handling to use merge commits

--- master/github_listener.py
+++ master/github_listener.py
@@ -44,3 +44,64 @@ class JuliaGithubListener(GitHubEventHandler):
 
         # Do some magic here
         return [change], 'git'
+
+
+    # We override handle_pull_request to use merge commits instead of branch tip sha's
+    # This requires a single changed line of code, highlighted below:
+    @defer.inlineCallbacks
+    def handle_pull_request(self, payload, event):
+        changes = []
+        number = payload['number']
+        refname = 'refs/pull/{}/{}'.format(number, self.pullrequest_ref)
+        basename = payload['pull_request']['base']['ref']
+        commits = payload['pull_request']['commits']
+        title = payload['pull_request']['title']
+        comments = payload['pull_request']['body']
+        repo_full_name = payload['repository']['full_name']
+        head_sha = payload['pull_request']['head']['sha']
+
+        log.msg('Processing GitHub PR #{}'.format(number),
+                logLevel=logging.DEBUG)
+
+        head_msg = yield self._get_commit_msg(repo_full_name, head_sha)
+        if self._has_skip(head_msg):
+            log.msg("GitHub PR #{}, Ignoring: "
+                    "head commit message contains skip pattern".format(number))
+            return ([], 'git')
+
+        action = payload.get('action')
+        if action not in ('opened', 'reopened', 'synchronize'):
+            log.msg("GitHub PR #{} {}, ignoring".format(number, action))
+            return (changes, 'git')
+
+        properties = self.extractProperties(payload['pull_request'])
+        properties.update({'event': event})
+        properties.update({'basename': basename})
+        change = {
+            ##################################################################
+            # THIS IS THE ONLY CHANGED LINE IN THE ENTIRE FUNCTION
+            'revision': payload['pull_request']['merge_commit_sha'],
+            ##################################################################
+            'when_timestamp': dateparse(payload['pull_request']['created_at']),
+            'branch': refname,
+            'revlink': payload['pull_request']['_links']['html']['href'],
+            'repository': payload['repository']['html_url'],
+            'project': payload['pull_request']['base']['repo']['full_name'],
+            'category': 'pull',
+            # TODO: Get author name based on login id using txgithub module
+            'author': payload['sender']['login'],
+            'comments': 'GitHub Pull Request #{0} ({1} commit{2})\n{3}\n{4}'.format(
+                number, commits, 's' if commits != 1 else '', title, comments),
+            'properties': properties,
+        }
+
+        if callable(self._codebase):
+            change['codebase'] = self._codebase(payload)
+        elif self._codebase is not None:
+            change['codebase'] = self._codebase
+
+        changes.append(change)
+
+        log.msg("Received {} changes from GitHub PR #{}".format(
+            len(changes), number))
+        return (changes, 'git')
