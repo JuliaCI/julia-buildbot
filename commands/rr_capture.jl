f45a7add5270a8d3dac748ea26a950a42dd9764c
Elliot Saba <staticfloat@gmail.com>
2020-04-22 13:24:49 -0700

    Update rr_capture.jl

--- commands/rr_capture.jl
+++ commands/rr_capture.jl
@@ -19,30 +19,32 @@ mktempdir() do dir
     rr() do rr_path
         new_env = copy(ENV)
         new_env["_RR_TRACE_DIR"] = joinpath(dir, "rr_traces")
-        new_env["JULIA_TEST_EXENAME"] = "$(rr_path) record --nested=detach julia"
+        new_env["JULIA_RR"] = "$(rr_path) record --nested=detach"
         t_start = time()
         proc = run(setenv(`rr record $ARGS`, new_env), (stdin, stdout, stderr); wait=false)
 
-        # Wait until either we timeout, or the process finishes
-        while (time() < t_start + TIMEOUT) && isopen(proc)
-            sleep(0.5)
-        end
+        # Start asynchronous timer that will kill `rr`
+        @async begin
+            sleep(TIMEOUT)
 
-        if isopen(proc)
-            println(stderr, "\n\nProcess timed out. Signalling `rr` for force-cleanup!")
-            kill(proc, Base.SIGTERM)
-            
-            @async begin
-                # Give `rr` one minute to clean things up
+            # If we've exceeded the timeout and `rr` is still running, kill it.
+            if isopen(proc)
+                println(stderr, "\n\nProcess timed out. Signalling `rr` for force-cleanup!")
+                kill(proc, Base.SIGTERM)
+                
+                # Give `rr` a chance to cleanup
                 sleep(60)
+
                 if isopen(proc)
-                    println(stderr, "\n\n`rr` failed to cleanup within one minute, killing!")
+                    println(stderr, "\n\n`rr` failed to cleanup within one minute, killing and exiting immediately!")
+                    kill(proc, Base.SIGKILL)
                     exit(1)
                 end
             end
         end
 
-        # If we got `rr` to finish properly, let's look at the exit code
+        # Wait for `rr` to finish, either through naturally finishing its run, or `SIGTERM`.
+        # If we have to `SIGKILL` 
         wait(proc)
 
         # On a non-zero exit code, upload the `rr` trace
