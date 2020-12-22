0104f0a4c8393e397d07191158849353c6da1811
Elliot Saba <staticfloat@gmail.com>
2020-12-22 00:11:31 +0000

    Finalize sauron alarm

--- master/slack_failure.py
+++ master/slack_failure.py
@@ -1,16 +1,26 @@
 from buildbot.process.results import FAILURE, EXCEPTION
-import logging
-
-log = logging.getLogger(__name__)
 
 def slack_failed_build(build):
-    log.warn(build)
-
     # Only report failure/exception (in reality this still reports but with an illegal payload)
     if build['results'] not in (FAILURE, EXCEPTION):
         return
 
+    # Only report if we have a URL:
+    if 'url' not in build:
+        return
+    url = build['url']
+
+    # Try to get the buildername:
+    builder_name = '<unknown builder>'
+    if 'builder' in build and 'name' in build['builder']:
+        builder_name = build['builder']['name']
+
+    # Try to get the worker name:
+    worker_name = '<unknown worker>'
+    if 'properties' in build and 'workername' in build['properties']:
+        worker_name = build['properties']['workername'][0]
+
     return {
-       'text': 'Builder %s on %s failed: %s'%(build['builder']['name'], 'placeholder', build['url']),
+       'text': 'Builder %s on %s failed: %s'%(builder_name, worker_name, url),
     }
 
