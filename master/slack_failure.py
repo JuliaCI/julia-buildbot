530aba89044a5071278e21c66952573ac0813334
Elliot Saba <staticfloat@gmail.com>
2020-12-22 00:00:22 +0000

    more debugging

--- master/slack_failure.py
+++ master/slack_failure.py
@@ -4,11 +4,12 @@ import logging
 log = logging.getLogger(__name__)
 
 def slack_failed_build(build):
+    log.warn(build)
+
     # Only report failure/exception (in reality this still reports but with an illegal payload)
     if build['results'] not in (FAILURE, EXCEPTION):
         return
 
-    log.info(build)
     return {
        'text': 'Builder %s on %s failed: %s'%(build['builder']['name'], 'placeholder', build['url']),
     }
