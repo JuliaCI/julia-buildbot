45e7cfebed3f22573eac61e577e22d759609c4de
Dilum Aluthge <dilum@aluthge.com>
2021-04-29 23:57:20 -0400

    If the working directory is not clean after `make install`, fail the build

--- master/package.py
+++ master/package.py
@@ -81,6 +81,14 @@ julia_package_factory.addSteps([
         sigtermTime=10,
         env=julia_package_env,
     ),
+    
+    # Check that the working directory is clean
+    steps.ShellCommand(
+        name="test that working directory is clean",
+        command=["bash", "-c", "if [ -z "$(git status --short)" ]; then   echo "INFO: The working directory is clean."; else   echo "ERROR: The working directory is dirty.";   echo "Output of git status:";   git status;   exit 1; fi"],
+        flunkOnFailure=True,
+        env=julia_package_env,
+    ),
 
     # Get info about ccache
     steps.ShellCommand(
